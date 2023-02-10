33cb2356a8b046366de667e25351610c16141970
Andrei Burkhanov <burkhanov.andrei@sonakai.com>
2023-02-10 15:48:30 +0200

    Create project

---
+++ FillingTwo-dimensionalArrays/FillingTwo-dimensionalArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FillingTwo_dimensionalArrays // Note: actual namespace depends on the project name.
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
